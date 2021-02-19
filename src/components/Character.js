aab1e189b8d2eebe0b3d978f6af60147c1dcb476
Alexis Marroquin <alexismarroquin754@gmail.com>
2021-02-19 11:29:31 -0800

    progress

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,11 +1,24 @@
 // Write your Character component here
-import React from 'react';
+import React, {useState} from 'react';
+import axios from 'axios';
+
+
 const Character = (props) => {
 
     const {characters} = props;
 
+    const Card = () => {
+        if(characters){
+            return <ul>{}</ul>
+        }    
+        return <div>No characters loaded</div>
+    }
+
+
     return (
         <div className="Character">
+            <h1>Character</h1>
+            <Card />
         </div>
     )
 }
