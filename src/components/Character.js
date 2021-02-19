e6fa1fa095daef12b2372c750d7211d9a0922786
Alexis Marroquin <alexismarroquin754@gmail.com>
2021-02-19 08:29:10 -0800

    axios request

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,28 @@
 // Write your Character component here
+import React from 'react';
+const Character = (props) => {
+
+    // const {name} = props;
+
+    // console.log(name)
+
+    // console.log(characters)
+    // console.log(makeCard)
+
+    return (
+        <div>
+            Characters
+        </div>
+    )
+    // characters.map(character => {
+    //     return (
+    //         <div className="character">
+    //             <h1>
+    //                 {character.name}
+    //             </h1>
+    //         </div>
+    //     )
+    // });
+}
+
+export default Character;
